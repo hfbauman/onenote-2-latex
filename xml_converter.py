1e3d679e2e565267f6dc2c1de927dd702423794b
hfbauman <60202942+hfbauman@users.noreply.github.com>
2025-03-06 13:37:50 -0500

    Use simple test case

--- xml_converter.py
+++ xml_converter.py
@@ -1,24 +1,31 @@
 #!/usr/bin/env python3
 import os
+from mathml2latex import mathml2latex
+import re
 
-def process_element(element):
+onenote_namespace = "{http://schemas.microsoft.com/office/onenote/2013/onenote}"
+
+def process_element(element,output):
     for child in element:
         if child.tag == onenote_namespace + "OEChildren":
-            process_element(child)
+            process_element(child,output)
         elif child.tag == onenote_namespace + "OE":
-            if 'quickStyleIndex' in child.attrib and child.attrib['quickStyleIndex'] == '1':
-                print("## " + child.find(onenote_namespace + "T").text)
-            elif 'quickStyleIndex' in child.attrib and child.attrib['quickStyleIndex'] == '3':
-                print("### " + child.find(onenote_namespace + "T").text)
-            else:
-                process_element(child)
+            # if 'quickStyleIndex' in child.attrib and child.attrib['quickStyleIndex'] == '1':
+            #     print("## " + child.find(onenote_namespace + "T").text)
+            # if 'quickStyleIndex' in child.attrib and child.attrib['quickStyleIndex'] == '3':
+            #     print("### " + child.find(onenote_namespace + "T").text)
+            # else:
+            process_element(child,output)
         elif child.tag == onenote_namespace + "T":
             if child.text:
-                print(child.text)
+                #Preprocess to remove xml tags
+                text = re.sub(r'<span.*?>|</span>', '', child.text)
+                # print(mathml2latex.convert(text))
+                output.write(mathml2latex.convert(text))
 
 
-input_filename=current_directory = os.path.dirname(os.path.realpath(__file__))+os.sep+"Lecture 9.xml"
-output_filename=current_directory+"Lecture 9 converted.md"
+input_filename=current_directory = os.path.dirname(os.path.realpath(__file__))+os.sep+"trial.xml"
+output_filename=current_directory+"trial.md"
 
 input_file = open(input_filename, "r", encoding="utf-8")
 input = input_file.read()
@@ -26,17 +33,16 @@ input_file.close()
 
 import xml.etree.ElementTree as ET
 
-onenote_namespace = "{http://schemas.microsoft.com/office/onenote/2013/onenote}"
-
 tree = ET.parse(input_filename)
 root = tree.getroot()
 
-for section in root:
-    if section.tag == onenote_namespace + "Title":
-        for child in section.iter():
-            if child.tag == onenote_namespace + "T":
-                print("# "+child.text)
+with open(output_filename, 'w') as output:
+    for section in root:
+        if section.tag == onenote_namespace + "Title":
+            for child in section.iter():
+                if child.tag == onenote_namespace + "T":
+                    output.write("# " + child.text + "\n")
 
-    elif section.tag == onenote_namespace + "Outline":
-        for attribute in section:
-            process_element(attribute)
+        elif section.tag == onenote_namespace + "Outline":
+            for attribute in section:
+                process_element(attribute,output)
