8082c37a15bffd0626e1c61fb8940e3cf2b98049
hfbauman <60202942+hfbauman@users.noreply.github.com>
2025-03-06 10:07:04 -0500

    Remove leading tabs before running the converter

--- driver.py
+++ driver.py
@@ -1,6 +1,9 @@
 import os
 from mathml2latex import mathml2latex
 
+def remove_leading_tabs(file):
+    return "\n".join([line[4:] if line.startswith("    ") else line for line in file.splitlines()])
+
 current_directory = os.path.dirname(os.path.realpath(__file__))+os.sep
 
 input_filename=current_directory+"Lecture 9.md"
@@ -9,6 +12,7 @@ output_filename=current_directory+"Lecture 9 converted.md"
 input_file = open(input_filename, "r", encoding="utf-8")
 input = input_file.read()
 input_file.close()
+input = remove_leading_tabs(input)
 output = mathml2latex.convert(input)
 output_file = open(output_filename, "w", encoding="utf-8")
 output_file.write(output)
